e574fcbe3e559bdbda2219c0be02bc557a4d9be2
Joe Gallotta <joeygallotta@gmail.com>
2018-11-20 20:32:13 -0600

    add navbar and display products

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,42 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Products</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Auth
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Signup</a>
+          <a class="dropdown-item" href="#">Login</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Logout</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
     <router-view/>
   </div>
+</div>
 </template>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p v-for="product in products"> {{product.name}}</p>
+    <!-- <p v-for="product in products"> {{product.name}}</p> -->
+     <div class="container">
+      <div class="row">
+        <div class="col-sm-4" v-for="product in products">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">name: {{ product.name }}</h5>
+              <p class="card-text">price: {{ product.price }}</p>
+              <img v-bind:src="product.image_url" width="100px">
+              <p class="card-text">description: {{product.description}}</p>
+              <a href="/show.html" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
